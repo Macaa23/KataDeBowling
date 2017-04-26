d9a243408e967a1df8e4c8e0de315b944b048611
Rocio Sepulveda <rocio_23s@hotmail.com>
2017-04-26 12:34:22 -0300

    Cuarto Test Errado

--- src/test/java/cl/ubb/testing/TestGame.java
+++ src/test/java/cl/ubb/testing/TestGame.java
@@ -44,4 +44,32 @@ public class TestGame {
 		assertEquals(60,result);
 		
 	}
+	
+	@Test
+	public void realGame(){
+		Game g = new Game();
+		int result;
+		g.roll(3);
+		g.roll(5);
+		g.roll(10);
+		g.roll(3);
+		g.roll(7);
+		g.roll(8);
+		g.roll(10);
+		g.roll(10);
+		g.roll(6);
+		g.roll(2);
+		g.roll(5);
+		g.roll(4);
+		g.roll(7);
+		g.roll(3);
+		g.roll(10);
+		g.roll(6);
+		g.roll(3);
+		
+		result = g.score();
+		
+		assertEquals(155, result);
+		
+	}
 }
